e4d7c6111e105a723dc72fa8cdf6ab610d7a3007
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-19 17:06:14 +0100

    to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,9 +3,9 @@
     returns an annotated tuple
 """
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns a tuple with str ad float'''
     return (k, v**2)
