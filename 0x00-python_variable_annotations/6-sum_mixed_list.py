358ac6abf39dd8caa092f535dc48586a3b10f9a8
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-19 17:08:33 +0100

    fixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,12 @@
 """_summary_
     sums a list of ints and floats
 """
+
+
 from typing import Union
 from math import fsum
+
+
 def sum_mixed_list(mxd_lst: (Union[int, float])) -> float:
+    '''return fsum(mxd_lst)'''
     return fsum(mxd_lst)
