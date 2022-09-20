e0de6a379b645c634ec8111fa45bb32a4aa71db1
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-20 23:46:30 +0100

    asyncio.gather

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-'''use task_wairèrandom
+'''
+use task_wait_n with tasks
 '''
 
 from typing import List
 
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''execute max_delay n times'''
-    tasks = [asyncio.create_task(wait_random(max_delay)) for x in range(n)]
-    res = await asyncio.gather(*tasks)
-    return res
+    r = []
+    tasks = [task_wait_random(max_delay) for x in range(n)]
+    for t in asyncio.as_completed(tasks):
+        await t
+    r.append(t)
+    return r
