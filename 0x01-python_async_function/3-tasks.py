a45a6c22182bae33a37fe37a9d2b6421d4d8b08e
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-20 22:15:58 +0100

    first commit

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+make an asyncio task
+'''
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    '''return task with asyncio'''
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
