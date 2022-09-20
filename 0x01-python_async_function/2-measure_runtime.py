d3611c97aa363f944bfcdc60f821d334a03cd6dc
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-20 23:22:36 +0100

    runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+measure performance of asynchronous exec
+'''
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    t_time = 0
+    s_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    t_time = time.perf_counter() - s_time
+    return t_time / n
