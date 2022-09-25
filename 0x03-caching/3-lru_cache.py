3f6a613bb139857563afc99fed9d04827f2c1460
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-25 01:29:16 +0100

    corrected lru condition

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -20,9 +20,9 @@ class LRUCache(BaseCaching):
         if(not key or not value):
             return
         self.cache_data[key] = value
-        if(len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS):
+        if(len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS):
             k = self.cache_data.popitem(last=False)[0]
-            print('Discard: {}'.format(k))
+            print('DISCARD: {}'.format(k))
             self.cache_data.move_to_end(key)
 
     def get(self, key):
