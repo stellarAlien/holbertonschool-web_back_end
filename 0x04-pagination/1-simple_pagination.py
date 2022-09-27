4d9f7ccdd52039e287c443d01bab84aef3cdf409
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-27 13:13:45 +0100

    pagintaion

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -32,6 +32,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''return  list of rows(lsits)'''
-        assert page > 0 and page_size > 0
+        ds = __class__.dataset(self)
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         start_idx, en_idx = index_range(page, page_size)
-        return __class__.dataset(self)[start_idx: en_idx]
+        return ds[start_idx:en_idx]
