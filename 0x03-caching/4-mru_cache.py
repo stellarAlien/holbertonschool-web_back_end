4cac797a8da347a664d6f65ab20be22707fdc5c2
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-29 12:34:26 +0100

    doesn't work properly

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -20,16 +20,17 @@ class MRUCache(BaseCaching):
         self.cache_data[key] = item
         if(len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS):
             k = self.cache_data.popitem(last=False)[0]
-            print('Discard: {}'.format(k))
+            print('DISCARD: {}'.format(k))
             self.cache_data.move_to_end(key, last=False)
-
+            
+        
     def get(self, key):
         '''get method WITH OrderedDict MECHANIC'''
         if (not key):
             return
         try:
             r = self.cache_data.get(key)
-            self.cache_data.move_to_end(key)
+            self.cache_data.move_to_end(key, last=False)
             return r
         except Exception:
             return
