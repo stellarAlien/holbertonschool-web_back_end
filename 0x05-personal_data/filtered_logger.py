c90f643f6e65e21338fc83119bd26aec30ad8b34
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-30 23:40:11 +0100

    learned about LogRecord attributes

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,13 +2,30 @@
 """
 filtered logger module
 """
-
+import logging
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    '''obfuscates log messages'''
-    for field in fields:
-        message = re.sub(f"{field}=(.*?;)",f"{field}={redaction}{separator}", message)
-    return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+        
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = self.filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+    def filter_datum(self, fields: List[str], redaction: str, message: str, separator: str) -> str:
+        '''obfuscates log messages'''
+        for field in fields:
+            message = re.sub(f"{field}=(.*?;)",f"{field}={redaction}{separator}", message)
+        return message
