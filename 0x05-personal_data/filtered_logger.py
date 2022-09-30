cffd60c4151093735d78c6ee42e096709d6d5b8d
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-30 22:21:46 +0100

    duck typing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,9 +4,10 @@ filtered logger module
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     '''obfuscates log messages'''
     for field in fields:
         message = re.sub(f"{field}=(.*?;)",f"{field}={redaction}{separator}", message)
