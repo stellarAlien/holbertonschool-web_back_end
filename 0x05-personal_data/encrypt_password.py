fb8164bac1f71ca24fc769ca5fb266de47fc9adc
root <root@DESKTOP-UKA4A0D.localdomain>
2022-10-02 15:53:13 +0100

    fixed mypy error

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,14 +9,12 @@ import logging
 
 def hash_password(password: str) -> ByteString:
     '''hash and salt a  password '''
-    password = password.encode('utf-8')
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     '''check if password provided is the password stored'''
-    password = password.encode('utf-8')
-    if bcrypt.checkpw(password, hashed_password):
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
         return True
     return False
