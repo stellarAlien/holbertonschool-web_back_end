1546dd15e08886a810b92bdf2795180864656655
root <root@DESKTOP-UKA4A0D.localdomain>
2022-10-02 15:49:01 +0100

    check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,4 +12,11 @@ def hash_password(password: str) -> ByteString:
     password = password.encode('utf-8')
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
-    
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''check if password provided is the password stored'''
+    password = password.encode('utf-8')
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    return False
