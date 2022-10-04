124398429a431a20912ecf71508277ac77faf328
root <root@DESKTOP-UKA4A0D.localdomain>
2022-10-04 21:59:57 +0100

    changed permissions for files

--- 0x06-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x06-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,8 +1,8 @@
-
 #!/usr/bin/env python3
 from flask import jsonify, abort
 from api.v1.views import app_views
 from models.user import User
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -11,14 +11,15 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
- 
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
       - the number of each objects
     """
-    
+
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
@@ -26,11 +27,11 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def error_page():
-  '''send abort signal'''
-  abort(401, description="Unauthorized")
+    '''send abort signal'''
+    abort(401, description="Unauthorized")
+
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden_view():
-      '''forbidden signal'''
-      abort(403, description="forbidden")
-      
+    '''forbidden signal'''
+    abort(403, description="forbidden")
