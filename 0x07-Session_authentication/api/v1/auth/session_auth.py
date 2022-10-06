3e39d838c1d31abb3e28b41973e16e52ad7d6ebd
stellarAlien <saifgaida@gmail.com>
2022-10-06 13:13:47 +0100

    get user by session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 '''
 session auth module
 '''
+
 from api.v1.auth.auth import Auth
 import uuid
 
@@ -11,8 +12,9 @@ class SessionAuth(Auth):
     '''session auth mechanism'''
 
     def __init__(self) -> None:
+        '''instantiate an instance'''
         super().__init__()
-        self.user_id_by_session_id = {}
+        self.user_id_by_session_id = dict()
 
     def create_session(self, user_id: str = None) -> str:
         '''create a session id for a user id'''
@@ -23,3 +25,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''return user of session'''
+        if not session_id or type(session_id) is not str:
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
