96a47f55914d90c3855e602f700ef56053375070
stellarAlien <saifgaida@gmail.com>
2022-10-06 12:55:43 +0100

    user_id type verif

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
         '''create a session id for a user id'''
         if user_id is None:
             return None
-        if type(user_id) != str:
+        if not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
