2fd0527366e0bc28d51fd30b69a4d7b31a654481
stellarAlien <saifgaida@gmail.com>
2022-10-06 12:51:06 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+'''
+session auth module
+'''
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     '''session auth mechanism'''
-    
+
     def __init__(self) -> None:
         super().__init__()
         self.user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         '''create a session id for a user id'''
         if user_id is None:
