2fdd9cb3f2662a3a04980695f1f370307b8a78d9
stellarAlien <saifgaida@gmail.com>
2022-10-06 18:41:29 +0100

    fixed view and added logout

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -44,7 +44,9 @@ def login():
     response.set_cookie(SESSION_NAME, new_session)
     return response
 
-@app_views.route('/api/v1/auth_session/logout', methods=["DELETE"], strict_slashes=False)
+
+@app_views.route('/api/v1/auth_session/logout', methods=["DELETE"],
+                 strict_slashes=False)
 def logout():
     '''delete session attached to request'''
     from api.v1.app import auth
@@ -52,4 +54,3 @@ def logout():
     if destroyed_sess:
         return jsonify({}), 200
     return False, abort(404)
-    
