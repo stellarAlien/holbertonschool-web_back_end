806772b7b0723b31734773b3ef53e98f51543266
stellarAlien <saifgaida@gmail.com>
2022-11-03 14:44:03 +0100

    removed casting to Year Type

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, Year(COALESCE(split, 2022) - formed) AS lifespan
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE 'Glam rock' 
+WHERE style LIKE '%Glam rock%' 
 ORDER By lifespan DESC;
