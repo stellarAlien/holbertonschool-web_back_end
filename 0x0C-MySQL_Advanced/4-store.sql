e8810a4bc26b3533fd3c5ada587dc1ce14c425c5
stellarAlien <saifgaida@gmail.com>
2022-11-03 16:03:05 +0100

    fixing trigger

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -3,8 +3,8 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS sell_trigger;
 CREATE TRIGGER  sell_trigger
 AFTER INSERT
-ON holberton.orders
+ON orders
 FOR EACH Row
 BEGIN
-    update items set quantity = quantity - NEW.number Where items.name = NEW.item_name;
+    update items set quantity = quantity - NEW.number Where name = NEW.item_name;
 END; $$ 
