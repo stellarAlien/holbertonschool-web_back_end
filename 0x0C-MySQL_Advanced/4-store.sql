cc217a1610538c7d0ce9099ad1edc946cfcc584a
stellarAlien <saifgaida@gmail.com>
2022-11-03 16:00:38 +0100

    fixing trigger

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,9 +1,10 @@
 -- create a trigger that decreases quantity of item
 DELIMITER $$
-DROP TRIGGER IF EXISTS holberton.`decr_quan`;
-CREATE TRIGGER  `decr_quan`
+DROP TRIGGER IF EXISTS sell_trigger;
+CREATE TRIGGER  sell_trigger
 AFTER INSERT
-ON holberton.orders FOR EACH Row
+ON holberton.orders
+FOR EACH Row
 BEGIN
     update items set quantity = quantity - NEW.number Where items.name = NEW.item_name;
 END; $$ 
