3404d496046be1496439b157cc4b5e04e3d16958
stellarAlien <saifgaida@gmail.com>
2022-11-22 23:37:33 +0100

    quick fix

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ export default function getListStudents() {
     let students = [
     {firstName: 'Guillaume', id: 1, location: 'San Francisco'},
     {firstName: 'James', id: 2, location: 'Columbia'},
-    {fistName: Serena, id: 5, location: 'San Francisco'},
+    {fistName: 'Serena', id: 5, location: 'San Francisco'},
     ];
 
     let arrayObject = Object.values(students); 
