b4a9d0f8a01e5a0745c17ae7f6600bcfef4a4613
stellarAlien <saifgaida@gmail.com>
2022-11-22 23:39:51 +0100

    trying babel linter

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,15 @@
-export default function groceriesList() {
-    const newMap = new Map();
-    newMap.set('Apples', 10);
-    newMap.set('Tomatoes', 10);
-    newMap.set('Pasta', 1);
-    newMap.set('Rice', 1);
-    newMap.set('Banana', 5);
-    return newMap;
-  }
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = groceriesList;
+function groceriesList() {
+  const newMap = new Map();
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
+  return newMap;
+}
