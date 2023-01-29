06ebbd70c7ece68ed345e27c60702c6c3bf758f3
stellarAlien <saifgaida@gmail.com>
2023-01-29 22:52:02 +0100

    formatter

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,19 +1,24 @@
-const http = require('http');
-const countStudents = require('./3-read_file_async');
+const http = require("http");
+const countStudents = require("./3-read_file_async");
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  if (req.url === "/") {
+    res.end("Hello Holberton School!");
+  } else if (req.url === "/students") {
     countStudents(process.argv[2])
       .then((data) => {
         let num = 0;
-        let response = '';
+        let response = "";
         for (const [key, value] of Object.entries(data)) {
           num += value.length;
-          response += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+          response += `Number of students in ${key}: ${
+            value.length
+          }. List: ${value.join(", ")}\n`;
         }
-        response = `This is the list of our students\nNumber of students: ${num}\n${response.slice(0, -1)}`;
+        response = `This is the list of our students\nNumber of students: ${num}\n${response.slice(
+          0,
+          -1
+        )}`;
         res.end(response);
       })
       .catch((error) => {
