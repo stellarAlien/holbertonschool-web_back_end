0951a5bba9d19653a4cadbf182acca8952cc5075
stellarAlien <saifgaida@gmail.com>
2023-01-29 22:33:42 +0100

    prettier other formatter on my vscode

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,40 @@
-const fs = require ('fs');
+const fs = require("fs");
 
-function countStudents (path) {
-  return new Promise ((resolve, reject) => {
-    fs.readFile (path, 'utf8', (err, datatext) => {
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, datatext) => {
       if (err) {
-        reject (Error ('Cannot load the database'));
+        reject(Error("Cannot load the database"));
       } else {
-        const lines = datatext.split (/\r?\n/);
+        const lines = datatext.split(/\r?\n/);
         const data = [];
         for (const i of lines) {
-          if (i !== '') {
-            data.push (i.split (','));
+          if (i !== "") {
+            data.push(i.split(","));
           }
         }
-        const first_name = data[0].indexOf ('firstname');
-        const field = data[0].indexOf ('field');
+        const first_name = data[0].indexOf("firstname");
+        const field = data[0].indexOf("field");
         const dict = {};
         for (const line of data) {
-          if (data.indexOf (line) !== 0) {
+          if (data.indexOf(line) !== 0) {
             if (line[field] in dict) {
-              dict[line[field]].push (line[first_name]);
+              dict[line[field]].push(line[first_name]);
             } else {
               dict[line[field]] = [];
-              dict[line[field]].push (line[first_name]);
+              dict[line[field]].push(line[first_name]);
             }
           }
         }
-        console.log (`Number of students: ${data.length - 1}`);
-        for (const [key, value] of Object.entries (dict)) {
-          console.log (
-            `Number of students in ${key}: ${value.length}. List: ${value.join (', ')}`
+        console.log(`Number of students: ${data.length - 1}`);
+        for (const [key, value] of Object.entries(dict)) {
+          console.log(
+            `Number of students in ${key}: ${value.length}. List: ${value.join(
+              ", "
+            )}`
           );
         }
-        resolve (dict);
+        resolve(dict);
       }
     });
   });
