ae531d338d9780d0e6de1e67a4e69181861752dd
stellarAlien <saifgaida@gmail.com>
2023-01-29 22:36:07 +0100

    typescript and js formatter

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, datatext) => {
+    fs.readFile(path, 'utf8', (err, datatext) => {
       if (err) {
-        reject(Error("Cannot load the database"));
+        reject(Error('Cannot load the database'));
       } else {
         const lines = datatext.split(/\r?\n/);
         const data = [];
         for (const i of lines) {
-          if (i !== "") {
-            data.push(i.split(","));
+          if (i !== '') {
+            data.push(i.split(','));
           }
         }
-        const first_name = data[0].indexOf("firstname");
-        const field = data[0].indexOf("field");
+        const first_name = data[0].indexOf('firstname');
+        const field = data[0].indexOf('field');
         const dict = {};
         for (const line of data) {
           if (data.indexOf(line) !== 0) {
@@ -29,9 +29,7 @@ function countStudents(path) {
         console.log(`Number of students: ${data.length - 1}`);
         for (const [key, value] of Object.entries(dict)) {
           console.log(
-            `Number of students in ${key}: ${value.length}. List: ${value.join(
-              ", "
-            )}`
+            `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`
           );
         }
         resolve(dict);
